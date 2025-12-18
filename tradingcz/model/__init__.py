b8de2b6a45be3a89a98ff74ff0c1ceae71e0c547
bx797 <martin.mizera@deutsche-boerse.com>
2025-12-18 14:49:53 +0100

    refactor: defer serde imports until implementation is complete

--- tradingcz/model/__init__.py
+++ tradingcz/model/__init__.py
@@ -4,12 +4,10 @@ This package provides:
 - Enums: Timeframe, Adjustment, SortOrder, OrderSide, OrderType
 - Domain Models: Bar, Quote, Trade, Snapshot (pure dataclasses)
 - Response Models: HTTP responses and Kafka keys
-- Serialization: JSON/CSV functions for domain models
 
 Usage:
     from tradingcz.model import Timeframe, Bar, Quote
     from tradingcz.model.response import BarResponse, QuoteResponse
-    from tradingcz.model.serde import bar_to_dict, bar_from_dict
 """
 
 from .enums import (
@@ -39,27 +37,6 @@ from .response import (
     RawSignalKey,
 )
 
-from .serde import (
-    bar_from_dict,
-    bar_from_json,
-    bar_to_dict,
-    bar_to_json,
-    quote_from_dict,
-    quote_from_json,
-    quote_to_dict,
-    quote_to_json,
-    trade_from_dict,
-    trade_from_json,
-    trade_to_dict,
-    trade_to_json,
-    snapshot_from_dict,
-    snapshot_from_json,
-    snapshot_to_dict,
-    snapshot_to_json,
-    bars_from_csv,
-    bars_to_csv,
-)
-
 __all__ = [
     # Enums
     "Timeframe",
@@ -84,23 +61,4 @@ __all__ = [
     "MarketStockQuoteKey",
     "MarketStockTradeKey",
     "RawSignalKey",
-    # Serde
-    "bar_to_dict",
-    "bar_from_dict",
-    "bar_to_json",
-    "bar_from_json",
-    "quote_to_dict",
-    "quote_from_dict",
-    "quote_to_json",
-    "quote_from_json",
-    "trade_to_dict",
-    "trade_from_dict",
-    "trade_to_json",
-    "trade_from_json",
-    "snapshot_to_dict",
-    "snapshot_from_dict",
-    "snapshot_to_json",
-    "snapshot_from_json",
-    "bars_to_csv",
-    "bars_from_csv",
 ]
